e28f6df2de21851eca2ab881bde127b6381bf7cb
Nelly <$>
2023-01-24 13:39:59 -0500

    created setToken function that Initially places the tokens in the last row. if no place available will default to decrement the count until it finds an open slot. checks for wins and weather or not the game board is full

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// DOM reference
+// DOM reference / Global 
 const container = document.querySelector(".container")
 const playerTurn = document.getElementById("playerTurn")
 const startScreen = document.querySelector(".startScreen")
@@ -20,7 +20,35 @@ let currentPlayer
 const selectRandomNumber = (min, max) =>
 Math.floor(Math.random() * (max - min)) + min
 
-//Placing token in box when user clicks on a box
+//set Token to exact points
+const setToken = (startCount, colValue) => {
+    let rows = document.querySelectorAll(".grid-row")
+    // Initially places the tokens in the last row. if no place available will default to decrement the count until it finds an open slot
+    if(initMatrix[startCount] [colValue] != 0) {
+        startCount -= 1
+        setToken(startCount, colValue)
+    } else {
+        //place token 
+        let currentRow = rows[startCount].querySelectorAll(".grid-box")
+        currentRow[colValue].classList.add("filled", `player${currentPlayer}`)
+        //update matrix
+        initMatrix[startCount][colValue] = currentPlayer
+        //Checks for wins
+        if (winCheck(startCount, colValue)) {
+            message.innerHTML = `Player<span> ${currentPlayer}</span> wins!`
+            startScreen.classList.remove("hide")
+            return false
+        }
+    }
+    // check if all slots are full 
+    gameOverCheck()
+
+}
+
+
+
+
+//User clicks -> Placing token in box 
 const fillBox = (e) => {
     //column value
     let colValue = parseInt(e.target.getAttribute("data-value"))
