09f339a49f4d41df6b062cdc1381897bb5679c20
Nelly <$>
2023-01-24 12:17:59 -0500

    created render matrix function

--- app.js
+++ app.js
@@ -12,3 +12,31 @@ let initMatrix = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
 ]
+
+let currentPlayer 
+
+//Selects a random number from given range
+const selectRandomNumber = (min, max) =>
+Math.floor(Math.random() * (max - min)) + min
+
+//Render Game Matrix
+const renderMatrix = () => {
+    for (let innerArray in initMatrix) {
+        let outerDiv = document.createElement("div")
+        outerDiv.classList.add("grid-row")
+        outerDiv.setAttribute("data-value", innerArray)
+        for (let j in initMatrix[innerArray]){
+            //sets matrix values to 0
+            initMatrix[innerArray][j] = [0]
+            let innerDiv = document.createElement("div")
+            innerDiv.classList.add("grid-box")
+            innerDiv.setAttribute("data-value", j)
+            innerDiv.addEventListener("click", (event) => {
+                fillBox(event)
+            })
+            outerDiv.appendChild(innerDiv)
+        }
+        container.appendChild(outerDiv)
+    }
+}
+
