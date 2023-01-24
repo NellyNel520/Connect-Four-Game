07e00dc7b1d5c44b03717061919a5d023ec15dcb
Nelly <$>
2023-01-24 11:59:36 -0500

    created inital matrix for game board

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const container = document.querySelector(".container")
 const playerTurn = document.querySelector("startScreen")
 const startButton = document.getElementById("start")
 const message = document.getElementById("message")
+
+let initMatrix = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+]
